262179eab6c7a201bae03c99774f579a27ae51d3
Xuan Yang <xuan.yang@goldenf.co>
2020-11-05 11:04:39 +0800

    update readme

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -23,7 +23,7 @@ class ConfigProvider
                 [
                     'id' => 'config',
                     'description' => 'The config of mongodb client.',
-                    'source' => __DIR__ . '/Publish/mongodb.php',
+                    'source' => __DIR__ . '/publish/mongodb.php',
                     'destination' => BASE_PATH . '/config/autoload/mongodb.php',
                 ],
             ],
