4d9aa0ee919e37ac87717ec4ebca634b95bc2af6
Xuan Yang <xuan.yang@goldenf.co>
2020-12-01 16:01:38 +0800

    fix config

--- src/Functions.php
+++ src/Functions.php
@@ -16,22 +16,7 @@ if (!function_exists('micro_timestamp')) {
 }
 
 if (!function_exists('mongodb_pool_config')) {
-    /**
-     * MongoDb 連結池
-     *
-     * @param $host
-     * @param string $dbName
-     * @param integer $port
-     * @param string $replica
-     * @param string $readPreference
-     * @param integer $maxConn
-     * @param float $connTimeout
-     * @param float $maxIdleTime
-     * @param string $username
-     * @param string $password
-     * @param string $authMechanism
-     * @return array
-     */
+    
 
      /**
       * $options = [
@@ -42,20 +27,45 @@ if (!function_exists('mongodb_pool_config')) {
       *     'readPreference' => $readPreference,
       * ],
       */
-    function mongodb_pool_config($host, string $dbName, int $port = 27017, array $options = [
-                                    'database' => 'admin',
-                                    'replica' => 'rs0',
-                                    'readPreference' => MongoDbConst::ReadPrefPrimary
-                                ],
-                                string $username = '', string $password = '',
-                                int $maxConn = 100, float $connTimeout = 10, float $maxIdleTime = 60 ): array {
+
+      /**
+       * MongoDb 連結池
+       *
+       * @param [type] $host
+       * @param string $dbName
+       * @param integer $port
+       * @param string $replica
+       * @param string $readPreference
+       * @param array $options
+       * @param string $username
+       * @param string $password
+       * @param integer $maxConn
+       * @param float $connTimeout
+       * @param float $maxIdleTime
+       * @return array
+       */
+    function mongodb_pool_config($host, string $dbName, int $port = 27017, string $replica = 'rs0', string $readPreference = 'primary', 
+        array $options = [
+            'database' => 'admin'
+        ],
+        string $username = '', string $password = '',
+        int $maxConn = 100, float $connTimeout = 10, float $maxIdleTime = 60 ): array {
+
+        $_options = array_merge(
+            $options,
+            [
+                'replica' => $replica,
+                'readPreference' => $readPreference
+            ]
+        );
+
         return [
             'username' => $username,
             'password' => $password,
             'host' => explode(';', $host),
             'port' => $port,
             'db' => $dbName,
-            'options'  => $options,
+            'options'  => $_options,
             'pool' => [
                 'min_connections' => 1,
                 'max_connections' => $maxConn,
