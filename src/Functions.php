e34c4cc90a8eae0c2b4dd57b6600fa1789266de5
Xuan Yang <xuan.yang@goldenf.co>
2020-12-02 14:26:21 +0800

    update funcs

--- src/Functions.php
+++ src/Functions.php
@@ -2,18 +2,6 @@
 
 declare(strict_types=1);
 
-use GiocoPlus\Mongodb\MongoDbConst;
-
-if (!function_exists('micro_timestamp')) {
-    /**
-     * 時間戳
-     *
-     * @return int
-     */
-    function micro_timestamp(): int {
-        return intval(round(microtime(true) * 1000));
-    }
-}
 
 if (!function_exists('mongodb_pool_config')) {
     
