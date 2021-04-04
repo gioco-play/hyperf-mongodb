bdc122a856c13b42c3134b688c5910636a634cdb
Xuan-Goldenf <40749214+Xuan-Goldenf@users.noreply.github.com>
2021-04-04 21:11:45 +0800

    Update mongodb.php

--- src/publish/mongodb.php
+++ src/publish/mongodb.php
@@ -14,7 +14,7 @@ return [
             //设置复制集,没有不设置
             'replica' => env('MONGODB_Replica', 'rs0'),
             'readPreference' => env('MONGODB_ReadPreference', 'primary'),
-            'connectTimeoutMS' => (int) env('MONGODB_connectTimeoutMS', 30)
+            // 'connectTimeoutMS' => (int) env('MONGODB_connectTimeoutMS', 30)
         ],
         'pool' => [
             'min_connections' => 3000,
